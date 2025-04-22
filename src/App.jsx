9a5c0082b620958db8141779c806712b47e26ef5
Gustavo Abranches Aranda <70330155+GustavoAbranches@users.noreply.github.com>
2025-04-22 16:11:22 +0000

    configura as pastas

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div className="w-full h-8 bg-red-800 flex items-center"></div>;
+};
+
+export default Footer;
