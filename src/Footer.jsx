69cbb693676ad5e2678001a112af2b53f34b4cd9
Gustavo Abranches Aranda <70330155+GustavoAbranches@users.noreply.github.com>
2025-04-22 19:54:24 +0000

    adiciona arquivos

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,5 +1,16 @@
+import Data from "./Footer_components/Data.jsx";
+import Hora from "./Footer_components/Hora.jsx";
+import Clima from "./Footer_components/Clima.jsx";
+
 const Footer = () => {
-  return <div className="w-full h-8 bg-red-800 flex items-center"></div>;
+  return (
+    <div className="w-full h-16 flex">
+      <Data />
+      <Hora />
+      <Clima day={0} text={'Hoje'}/>
+      <Clima day={1} text={'Amanhã'}/>
+    </div>
+  );
 };
 
 export default Footer;
