13a20382e7653358ae2d413ecd212f67a42b6486
Arthur <79378571+Tacini@users.noreply.github.com>
2025-04-25 18:19:02 +0000

    adicionado pasta pages, pagina login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./Footer";
 import BarraLateral from "./BarraLateral";
-import TelaPrincipal from "./TelaPrincipal"
+import TelaPrincipal from "./TelaPrincipal";
+import Login from "./pages/Login";
 
 function App() {
   return (
-     <div className="flex h-screen">
-      <TelaPrincipal />
-      <BarraLateral />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="*"
+          element={
+            <div className="flex h-screen">
+              <TelaPrincipal />
+              <BarraLateral />
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
