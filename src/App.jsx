1d2c81f14567952be936515b54c6ad0af9025f91
Arthur <79378571+Tacini@users.noreply.github.com>
2025-05-06 17:03:59 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Footer from "./Footer";
 import BarraLateral from "./BarraLateral";
 import TelaPrincipal from "./TelaPrincipal";
 import Login from "./pages/Login";
+import Aviso from "./Notice_components/Aviso";
 
 function App() {
   return (
@@ -13,8 +13,11 @@ function App() {
         <Route
           path="*"
           element={
-            <div className="flex h-screen">
-              <TelaPrincipal />
+            <div className="flex h-screen w-screen">
+              <div className="relative flex-1 flex flex-col">
+                <TelaPrincipal />
+                <Aviso />
+              </div>
               <BarraLateral />
             </div>
           }
