5c8a46ba005057061c55031b5f96719e0faf2846
Arthur <79378571+Tacini@users.noreply.github.com>
2025-05-27 17:23:36 +0000

    Adicionado Admin, Painel de Materias e DashBoard na pasta pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-//Import da telas
+import PainelMaterias from "./pages/PainelMaterias";
+import Dashboard from "./pages/Dashboard";
+import Admin from "./pages/Admin";
 import Login from "./pages/Login";
 import Horario from "./pages/Horario";
 import Tela from "./pages/Tela";
@@ -14,6 +16,9 @@ function App() {
         <Route path="/vinheta" element={<Vinheta />} />
         <Route path="/horario" element={<Horario />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/painel-materias" element={<PainelMaterias />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
