d4a8b9ef6aba4f58383f3088f84c97e9acfe0d84
Arthur <79378571+Tacini@users.noreply.github.com>
2025-05-27 20:00:14 +0000

    Adicionado apenas o estilo das linhas verticais

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Tela />} />
+        <Route path="/" element={<Login />} />
         <Route path="/vinheta" element={<Vinheta />} />
         <Route path="/horario" element={<Horario />} />
         <Route path="/materias" element={<Materias />} />
--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -2,8 +2,14 @@ import Sidebar from "../components/Sidebar";
 
 const Cadastro = () => {
   return (
+    <div className="flex flex-row ">
+        <div className="2xl:w-8 lg:w-4 bg-carmineRed" />
+        <div className="2xl:w-8 lg:w-4 bg-tangerine" />
+        <div className="2xl:w-8 lg:w-4 bg-mediumOrange" />
+      
     <div className="h-screen">
       <Sidebar />
+    </div>
     </div>
   );
 };
