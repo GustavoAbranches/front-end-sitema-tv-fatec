b10bcadd00e1e7563cb40ab5af54a7a75afd3d8a
gustavo <gusabranches@gmail.com>
2025-05-27 08:39:37 -0300

    conexão com a api, criação dos hooks e services

---
+++ src/hooks/useHorarios.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import {
+  getHorarios,
+  postHorario,
+  deleteHorario,
+} from "../services/horarioService";
+
+export function useHorario() {
+  const [horarios, setHorarios] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchHorarios = async () => {
+    setLoading(true);
+    try {
+      const data = await getHorarios();
+      console.log("Dados recebidos da API:", data);
+      setHorarios(data);
+    } catch (err) {
+      console.error("Erro na API:", err);
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const addHorario = async (novoHorario) => {
+    try {
+      const data = await postHorario(novoHorario);
+      setHorarios((prev) => [...prev, data]);
+    } catch (err) {
+      setError(err);
+    }
+  };
+
+  const removeHorario = async (id) => {
+    try {
+      await deleteHorario(id);
+      setHorarios((prev) => prev.filter((item) => item.id !== id));
+    } catch (err) {
+      setError(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchHorarios();
+  }, []);
+
+  return {
+    horarios,
+    loading,
+    error,
+    fetchHorarios,
+    addHorario,
+    removeHorario,
+  };
+}
